2f49a40f83bd318e7e7307735a010bafb5c1cadf
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-06-10 18:12:25 -0500

    finished

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,22 +4,28 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+myFunction = () => console.log("Function was invoked!")
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
-
+anotherFunction = (param) => param
+anotherFunction("Example");
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (p1,p2) => p1+ p2
+add(1,2)
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
+let subtract = (p1,p2) => p1- p2
+subtract(1,2)
 
 // Stretch
 
